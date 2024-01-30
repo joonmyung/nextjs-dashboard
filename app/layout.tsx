91664bc18206d7be52fe6780afeb19e8cca13598
Joonmyung <mytwiny@gmail.com>
2024-01-30 19:59:16 +0900

    Update dashboard and invoices pages and perform related modifications
    This commit provides a significant update to the dashboard and invoices pages. Multiple UI elements such as search, pagination, and table components have been added to provide a more interactive and functional user interface. Additionally, the commit also involves improving existing code by modifying the data fetching method and introducing code for user analytics. The changes also include version updates for dependencies in package.json and some adjustments in configuration files.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import '@/app/ui/global.css'
 import React from "react";
 import { notoSansKr } from "@/app/ui/fonts";
+import {Analytics} from "@vercel/analytics/react";
 
 export default function RootLayout({
   children,
@@ -9,7 +10,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
-      <body className={`${notoSansKr.className} antialiased`}>{children}</body>
+      <body className={`${notoSansKr.className} antialiased`}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
--- scripts/seed.js
+++ scripts/seed.js
@@ -12,7 +12,7 @@ async function seedUsers(client) {
     await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
     // Create the "users" table if it doesn't exist
     const createTable = await client.sql`
-      CREATE TABLE IF NOT EXISTS users (
+      CREATE TABLE IF NOT EXISTS dashboard_users (
         id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
         name VARCHAR(255) NOT NULL,
         email TEXT NOT NULL UNIQUE,
@@ -20,28 +20,28 @@ async function seedUsers(client) {
       );
     `;
 
-    console.log(`Created "users" table`);
+    console.log(`Created "dashboard_users" table`);
 
     // Insert data into the "users" table
     const insertedUsers = await Promise.all(
       users.map(async (user) => {
         const hashedPassword = await bcrypt.hash(user.password, 10);
         return client.sql`
-        INSERT INTO users (id, name, email, password)
+        INSERT INTO dashboard_users (id, name, email, password)
         VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
         ON CONFLICT (id) DO NOTHING;
       `;
       }),
     );
 
-    console.log(`Seeded ${insertedUsers.length} users`);
+    console.log(`Seeded ${insertedUsers.length} dashboard_users`);
 
     return {
       createTable,
-      users: insertedUsers,
+      dashboard_users: insertedUsers,
     };
   } catch (error) {
-    console.error('Error seeding users:', error);
+    console.error('Error seeding dashboard_users:', error);
     throw error;
   }
 }
